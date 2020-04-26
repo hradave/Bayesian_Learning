5cd3e5763e150d2298711617fe782c1273f7f64d
Oriol Garrobé <oriol.garrobe@gmail.com>
2020-04-26 19:39:45 +0200

    tiki

--- Lab2/Exercise 1 Oriol.R
+++ Lab2/Exercise 1 Oriol.R
@@ -1,14 +1,14 @@
+library("mvtnorm")
+
 # Linear and polynomial regression
 
 data <- read.table("TempLinkoping.txt", header = TRUE)
-View(data)
 
 y <- data$temp
 time <- data$time
 
 n <- length(y)
 X <- cbind(matrix(1, nrow = length(y)),time,time^2)
-View(X)
 
 # Initial prior hyperparameters
 mu_0 <- c(-10,100,-100)
@@ -17,15 +17,67 @@ nu_0 <- 4
 sigma2_0 <- 1
 
 # Conjugate prior
-draw_sigma2 <- nu_0*sigma2_0/rchisq(10, nu_0)
+compute_betas <- function(N_draws, mu_0, omega_0, nu_0, sigma2_0){
+  draw_sigma2 <- nu_0*sigma2_0/rchisq(Ndraws, nu_0)
+  draw_beta <- matrix(0, nrow=length(draw_sigma2), ncol=3 )
+  for (sigma in draw_sigma2) {
+    i <- which(draw_sigma2==sigma)
+    draw_beta[i,] <- rmvnorm(1, mu_0, sigma*omega_0)
+  }
+  return(colMeans(draw_beta))
+}
+
+Ndraws=1000
+
+# Tune hyperparameters
+
+# Initial hyperparameters
+beta_0 <- compute_betas(Ndraws, mu_0, omega_0, nu_0, sigma2_0)
+
+# 1st iteration
+mu_1 <- c(-5,90,-90)
+beta_1 <- compute_betas(Ndraws, mu_1, omega_0, nu_0, sigma2_0)
+
+# 2nd iteration
+mu_2 <- c(0,100,-100)
+beta_2 <- compute_betas(Ndraws, mu_2, omega_0, nu_0, sigma2_0)
+
+# 3rd iteration
+mu_3 <- c(0,85,-85)
+beta_3 <- compute_betas(Ndraws, mu_3, omega_0, nu_0, sigma2_0)
+
+# 4th iteration
+mu_4 <- c(-5, 85, -85)
+beta_4 <- compute_betas(Ndraws, mu_4, omega_0, nu_2, sigma2_0)
+
+plot(time, X %*% beta_0, type = 'l', lwd=2, col='red', ylim=c(-10, 25),
+     ylab='Temperature', xlab='Time')
+points(time, X %*% beta_1, lwd=2, col='blue', type='l')
+points(time, X %*% beta_2, lwd=2, col='pink', type='l')
+points(time, X %*% beta_3, lwd=2, col='green', type='l')
+points(time, X %*% beta_4, lwd=2, col='yellow', type='l')
+
+
+# We like blue
+
+beta_hat <- solve(t(X)%*%X)%*%t(X)%*%y
+mu_n <- solve(t(X)%*%X + omega_0) %*% (t(X)%*%X%*%beta_hat + omega_0%*%mu_0)
+omega_n <- t(X) %*% X + omega_0
+nu_n <- nu_0 + n # what is n?????????????????????
+nu_sigma2_n <- nu_0*sigma2_0+(t(y)%*%y + t(mu_0)%*%omega_0%*%mu_0 - t(mu_n)%*%omega_n%*%mu_n)
+sigma2_n <- nu_sigma2_n[1] / nu_n
+
+draw_sigma2 <- nu_n*sigma2_n/rchisq(Ndraws, nu_n)
 draw_beta <- matrix(0, nrow=length(draw_sigma2), ncol=3 )
 for (sigma in draw_sigma2) {
   i <- which(draw_sigma2==sigma)
-  draw_beta[i,] <- rmvnorm(1, mu_0, sigma*omega_0)
+  draw_beta[i,] <- rmvnorm(1, mu_n, sigma*omega_n)
 }
 
 
-## ???? How many?? ###
+
+
+
 
 #### X = rchisq(nDraws,n)
 #### sigma2_sim = n*tau2/X
